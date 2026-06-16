b563ab4abed052cadbedbb2f567806e5a90e9ed5
ruri <ruriyoshinova@proton.me>
2026-06-16 11:35:58 +0800

    update gradle configs

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    java
+    id("com.diffplug.spotless") version "8.6.0"
+}
+
+group = "one.ruri"
+version = "1.1.0"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "spigotmc-repo"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        name = "papermc-repo"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.70-stable")
+    compileOnly(files("libs/AuthMeReloaded-5.6.0.jar"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    from({
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes(
+            "Main-Class" to "one.ruri.authmeplus.AuthMePlus",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
+    }
+}
